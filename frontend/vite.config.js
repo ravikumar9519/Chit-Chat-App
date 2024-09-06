b5d77678be8c7616409dfeecb91754ac19b553e3
Ravi Bharti <118691241+ravikumar9519@users.noreply.github.com>
2024-09-06 09:56:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,16 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 3000,
+		port: 10000,
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://chit-chat-app-aoy3.onrender.com",
 			},
 		},
 	},
 });
-export default {
-  build: {
-    chunkSizeWarningLimit: 1000, // Size in KB
-  },
-}
